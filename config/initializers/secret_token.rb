90ec333040c93f1d64f3aae7c5a35f8fe41261a1
Zhen <di3@wisc.edu>
2014-02-10 11:25:35 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+POne::Application.config.secret_key_base = '8f4b9d93694f70eb6fc3c5aeb57c9b2b17a0a31102b76b465e68ef9b462c7fc3a5785869edf2c5ebf0e1a93bae8fb44a2ff16d130a011c5a69cce16ae8352da3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+POne::Application.config.session_store :cookie_store, key: '_p_one_session'
